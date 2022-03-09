b69cd2ba9ed76d7f8f2b105cac414fb76fcd4aa4
ebenKB <ekb.adjei@yahoo.com>
2022-03-09 07:19:59 +0000

    Add redux toolkit for state management

---
+++ src/features/event/EventSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const eventSlice = createSlice({
+  name: "event",
+  initialState: [{title: "test"}, {title: "test 2", description: "lorem ipsum igitu"}],
+  reducers: {
+    addEvents: (state, action)=> {
+      state = action.payload
+    }
+  }
+})
+
+
+export const { addEvents } = eventSlice.actions;
+export const selectEvents = (state) => state.event;
+
+export default eventSlice.reducer;
